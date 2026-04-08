c6c482516e3c05d79c0a063b04bff60a04561af0
teraflops <cprieto.ortiz@gmail.com>
2026-04-08 17:26:01 +0200

    Update to v1.8.4

--- Casks/drplayer.rb
+++ Casks/drplayer.rb
@@ -1,6 +1,6 @@
 cask "drplayer" do
-  version "1.8.3"
-  sha256 "039585e00cff845ec6187a7bcfbb4c1ed971a62e1c4b3760d8cb870c8c4398f5"
+  version "1.8.4"
+  sha256 "05c31d12fc8e53d248e26af3ac2275771aa17af96d06f325400b677a892c0c5d"
 
   url "https://drplayer.priet.us/downloads/DrPlayer-#{version}.dmg"
   name "DrPlayer"
