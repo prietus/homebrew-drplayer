52dec1743cc879900d9cc885cc7caa0bb4fd4c31
teraflops <cprieto.ortiz@gmail.com>
2026-04-12 02:47:45 +0200

    Update to v1.9.0

--- Casks/drplayer.rb
+++ Casks/drplayer.rb
@@ -1,6 +1,6 @@
 cask "drplayer" do
-  version "1.8.9"
-  sha256 "bb4fd8c31a545130e3b6d47fe1fff4d0535bd2fab7c4d8bfc5d15433af99e340"
+  version "1.9.0"
+  sha256 "a59e01388be794e6b6599dc8bd66e4270aafd197c4eb8bb03bf70c06f0450f8b"
 
   url "https://drplayer.priet.us/downloads/DrPlayer-#{version}.dmg"
   name "DrPlayer"
