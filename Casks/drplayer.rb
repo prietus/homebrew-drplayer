9c31fd9e3159d9839172b7ae9b3a426d4f235af1
teraflops <cprieto.ortiz@gmail.com>
2026-09-22 21:00:11 +0200

    drplayer 1.11.0

--- Casks/drplayer.rb
+++ Casks/drplayer.rb
@@ -1,6 +1,6 @@
 cask "drplayer" do
-  version "1.10.0"
-  sha256 "REPLACE_WITH_SHA256_OF_RELEASE_DMG"
+  version "1.11.0"
+  sha256 "5199ced37bffca9bf8100eec7789e4be84376a088e3dba7396f4e0a90d0fec04"
 
   url "https://github.com/prietus/drplayer/releases/download/v#{version}/DrPlayer-#{version}.dmg"
   name "DrPlayer"
